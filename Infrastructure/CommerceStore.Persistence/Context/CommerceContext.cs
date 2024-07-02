ec4869927d959844fbdeee7914d6e7c39d903f7c
MnrMine <mnr_mine_agri@hotmail.com>
2024-07-02 17:00:44 +0300

    Add project files.

---
+++ Infrastructure/CommerceStore.Persistence/Context/CommerceContext.cs
@@ -0,0 +1,20 @@
+﻿using CommerceStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommerceStore.Persistence.Context
+{
+    public class CommerceContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-HHOE249\\SQLEXPRESS;initial Catalog=CommerceStoreDb;integrated Security=true");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
